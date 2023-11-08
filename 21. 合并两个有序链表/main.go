df477493862d66351a7c64270d62a6a10e57c5cd
reck_wang <reck_wang@wxp.com>
2023-11-08 10:18:39 +0800

    完成21、23题

--- 21. 合并两个有序链表/main.go
+++ 21. 合并两个有序链表/main.go
@@ -1,1 +1,48 @@
-package _1__合并两个有序链表
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// recursion
+//func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+//	if list1 == nil {
+//		return list2
+//	}
+//
+//	if list2 == nil {
+//		return list1
+//	}
+//
+//	if list1.Val < list2.Val {
+//		list1.Next = mergeTwoLists(list1.Next, list2)
+//		return list1
+//	}
+//
+//	list2.Next = mergeTwoLists(list1, list2.Next)
+//	return list2
+//}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var dummyHead = &ListNode{}
+	var cur = dummyHead
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+		cur = cur.Next
+	}
+
+	if list1 != nil {
+		cur.Next = list1
+	} else {
+		cur.Next = list2
+	}
+
+	return dummyHead.Next
+}
--- 23. 合并 K 个升序链表/main.go
+++ 23. 合并 K 个升序链表/main.go
@@ -1,1 +1,58 @@
-package _3__合并_K_个升序链表
+package main
+
+import "container/heap"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	pq := &priorityQueue{nodes: []*ListNode{}}
+	for _, list := range lists {
+		if list != nil {
+			heap.Push(pq, list)
+		}
+	}
+
+	dummyHead := &ListNode{}
+	cur := dummyHead
+	for pq.Len() > 0 {
+		node := heap.Pop(pq).(*ListNode)
+		cur.Next = node
+
+		if node.Next != nil {
+			heap.Push(pq, node.Next)
+		}
+		cur = cur.Next
+	}
+
+	return dummyHead.Next
+}
+
+type priorityQueue struct {
+	nodes []*ListNode
+}
+
+func (p *priorityQueue) Len() int {
+	return len(p.nodes)
+}
+
+func (p *priorityQueue) Less(i, j int) bool {
+	return p.nodes[i].Val < p.nodes[j].Val
+}
+
+func (p *priorityQueue) Swap(i, j int) {
+	p.nodes[i], p.nodes[j] = p.nodes[j], p.nodes[i]
+}
+
+func (p *priorityQueue) Push(i interface{}) {
+	p.nodes = append(p.nodes, i.(*ListNode))
+}
+
+func (p *priorityQueue) Pop() interface{} {
+	temp := len(p.nodes) - 1
+	res := p.nodes[temp]
+	p.nodes = p.nodes[:temp]
+	return res
+}
