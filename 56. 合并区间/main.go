88a1889eb828ee259b6ecbe1d5453cf4766b1bd8
reck_wang <reck_wang@wxp.com>
2023-12-29 10:15:22 +0800

    完成56题

---
+++ 56. 合并区间/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	var ans [][]int
+	for _, interval := range intervals {
+		if len(ans) == 0 || ans[len(ans)-1][1] < interval[0] {
+			ans = append(ans, interval)
+		} else {
+			ans[len(ans)-1][1] = max(ans[len(ans)-1][1], interval[1])
+		}
+	}
+
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
